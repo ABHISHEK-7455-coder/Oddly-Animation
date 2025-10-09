370e4c1b2322ecf338c758d28cae2780b456abb7
Abhishek <abhishekkumardeshwal@gmail.com>
2025-10-09 21:12:12 +0530

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <main className="home-container">
       <h1 className="hero-title">Oddly Satisfying Animations</h1>
-      {/* <p className="hero-subtitle">Click on a card to see the animation.</p> */}
 
       {/* Gallery component */}
       <Gallery />
