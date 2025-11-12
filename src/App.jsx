96b07c7038a2a81e15f4e5201beb8b6c80c28129
Abhishek <abhishekkumardeshwal@gmail.com>
2025-11-12 23:14:35 +0530

    change gallery design

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
                         path="/" 
                         element={
                             <>
-                                {/* <AnimatedBackground /> */}
+                                <AnimatedBackground />
                                 <Header />
                                 <Home />
                             </>
@@ -65,7 +65,7 @@ function App() {
                         path="/about" 
                         element={
                             <>
-                                {/* <AnimatedBackground /> */}
+                                <AnimatedBackground />
                                 <Header />
                                 <About />
                             </>
