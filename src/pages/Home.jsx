38b4685c185b317b022b8d57805868cb4003d928
Abhishek <abhishekkumardeshwal@gmail.com>
2025-11-22 20:43:48 +0530

    add animation cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import HeroSection from "../components/HeroSection";
+import { animationData } from "../data/animationConfig";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   return (
     <main className="home-container">
       <HeroSection />
@@ -12,6 +16,19 @@ const Home = () => {
         <p className="animations-intro-subtitle ">
           Discover 50+ mesmerizing, interactive, and visually stunning animations created to bring peace and satisfaction.
         </p>
+
+        <div className="anim-cards">
+          {animationData.map((anim) => (
+            <div
+              className="anim-card"
+              onClick={() => navigate(`/gallery?anim=${anim.id}`)}
+            >
+              <img src={anim.image} width="200px" height="200px" />
+              <h3>{anim.name}</h3>
+            </div>
+
+          ))}
+        </div>
 
         {/* <div className="bubbles">
           <img src="src/assets/bubble.png" />
